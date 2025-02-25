383f938242aceeecb01b4347a3cb0c3b8a8c9690
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2025-02-25 09:00:20 -0500

    Bump version to 0.1.0 and tag for release

--- certbot_deployer/meta.py
+++ certbot_deployer/meta.py
@@ -3,7 +3,7 @@ Package metadata
 """
 
 __author__ = "IAS"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __title__ = "certbot_deployer"
 __prog__ = "certbot-deployer"
 __license__ = "License :: OSI Approved :: MIT License"
